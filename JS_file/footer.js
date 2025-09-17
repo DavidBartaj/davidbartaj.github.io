c3844befdc4a91fa383ab96269680e79dba2b23a
DavidBartaj <44649362+DavidBartaj@users.noreply.github.com>
2025-09-17 15:47:37 +0300

    Add files via upload

---
+++ JS_file/footer.js
@@ -0,0 +1,24 @@
+function footer() {
+    document.write(
+        `<footer id="contacts">
+    <div class="footerWrapper">
+        <div class="footerContentWrap1">
+            <a href = "mailto: microteamrakhiv@gmail.com">microteamrakhiv@gmail.com</a>
+        </div>
+        <div class="footerContentWrap2">
+            <li style="list-style: none" class="nawLi"><a class="nawLi" href="../index.html">
+                <img src="../img/logo.png" alt=""></a></li>
+
+        </div>
+        <div class="footerContentWrap3">
+            <a href="tel:+380679884216"><img src="../icons/telephone-symbol-button.png" alt="">+380679884216</a>
+            <a href="tel:+380674250254"><img src="../icons/telephone-symbol-button.png" alt="">+380674250254</a>
+            <a href="tel:+380930325868"><img src="../icons/telephone-symbol-button.png" alt="">+380930325868</a>
+                       <a href=""><img src="../icons/viber.png" alt="">+380987694307</a>
+        </div>
+    </div>
+</footer>`
+    )
+}
+
+footer();
---
+++ JS_file/footerMainPage.js
@@ -0,0 +1,23 @@
+function footer() {
+    document.write(`<footer id="contacts">
+    <div class="footerWrapper">
+        <div class="footerContentWrap1">
+		    <a href = "mailto: microteamrakhiv@gmail.com">microteamrakhiv@gmail.com</a>
+        </div>
+        <div class="footerContentWrap2">
+            <li style="list-style: none" class="nawLi"><a class="nawLi" href="#header">
+                <img src="img/logo.png" alt=""></a></li>
+
+        </div>
+        <div class="footerContentWrap3">
+            <a href="tel:+380679884216">+380679884216</a>
+         
+            <a href="tel:+380674250254">+380674250254</a>
+            <a href="tel:+380930325868">+380930325868</a>
+            <a href="">+380987694307</a>
+        </div>
+    </div>
+</footer>`)
+}
+
+footer();
